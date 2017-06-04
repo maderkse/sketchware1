40261059bc9b139a3e638be4c15b506feb020c35
maderkse <maderkse@gmail.com>
2017-06-04 18:28:23 +0200

    test

---
+++ app/src/main/java/com/my/helloworld/MainActivity.java
@@ -0,0 +1,69 @@
+package com.my.helloworld;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.graphics.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.util.*;
+import android.webkit.*;
+import java.util.*;
+import java.text.*;
+
+
+public class MainActivity extends Activity {
+
+	private TextView textview;
+
+
+
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		initialize();
+		initializeLogic();
+	}
+
+	private void  initialize() {
+		textview = (TextView) findViewById(R.id.textview);
+
+
+
+	}
+
+	private void  initializeLogic() {
+		textview.setText("Ik hou van Hanneke en El");
+	}
+
+
+
+
+	// created automatically
+	private void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+
+	private int getRandom(int _minValue ,int _maxValue){
+		Random random = new Random();
+		return random.nextInt(_maxValue - _minValue + 1) + _minValue;
+	}
+
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+				_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+
+}
